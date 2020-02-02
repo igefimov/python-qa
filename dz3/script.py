32c31e7d168e28394c4772075bb71766dc3dff27
Igor Efimov <igor.efimov@merck.com>
2020-02-02 21:05:51 +0100

    my script

--- dz3/script.py
+++ dz3/script.py
@@ -1,22 +1,36 @@
+#!/usr/bin/env python
+
 import json
 import csv
 
 myList = []
 tmpDict = {}
+books_list = []
+
+
+with open("books.csv", "r") as books_file:
+    next(csv.reader(books_file))
+
+    for row in csv.reader(books_file):
+        tmpDict['title'] = row[0]
+        tmpDict['autohor'] = row[1]
+        tmpDict['height'] = row[3]
+        books_list.append(tmpDict.copy())
+
+tmpDict.clear()
+
 with open("users.json", "r") as users_file:
+    i = 0
     users_data = json.load(users_file)
-    for i in users_data:
-        tmpDict["gender"] = i["gender"]
-        tmpDict["name"] = i["name"]
-        tmpDict["address"] = i["address"]
+    for user in users_data:
+        tmpDict["gender"] = user["gender"]
+        tmpDict["name"] = user["name"]
+        tmpDict["address"] = user["address"]
+        tmpDict["books"] = []
+        tmpDict["books"].append(books_list[i])
         myList.append(tmpDict)
-
-
+        i += 1
 print myList
-# with open("books.csv", "r") as books_file:
-#     books_data = csv.reader(books_file)
-#     for row in books_data:
-#         print row
-#         print type(row)
-#         break
 
+with open("new.json", "w") as my_file:
+    json.dump(myList, my_file)
