7375768708a02b2841e456f6c102bc97635c9813
Igor Efimov <igor.efimov@merck.com>
2020-03-21 23:05:27 +0100

    Added browser init and tearDown logging; Added browser Events logging

--- dz15-logging/conftest.py
+++ dz15-logging/conftest.py
@@ -1,13 +1,47 @@
-import pytest
+from datetime import datetime
 from selenium import webdriver
+from selenium.webdriver.support.events import EventFiringWebDriver, AbstractEventListener
+import logging
+import pytest
+
+logging.basicConfig(
+    filename="log/test.log",
+    format='%(asctime)s %(name)s [%(levelname)s]: %(message)s',
+    level=logging.INFO
+)
+logger = logging.getLogger('Fixture')
+
+
+class MyListener(AbstractEventListener):
+    def after_navigate_to(self, url, driver):
+        logger.info(f"I'm on {url}")
+
+    def after_find(self, by, value, driver):
+        logger.info(f"I've found '{value}' with '{by}'")
+
+    def before_click(self, element, driver):
+        logger.info(f"I'm clicking {element.text} {element.tag_name}")
+
+    def before_execute_script(self, script, driver):
+        logger.info(f"I'm executing '{script}'")
+
+    def after_close(self, driver):
+        logger.info(f"===================== Finita la comedia =====================")
+
+    def on_exception(self, exception, driver):
+        logger.error(f'Houston, we have a problem : {exception}')
+        driver.save_screenshot("screenshot/ERR-{0}.png".format(datetime.now().strftime("%d-%b-%Y %H:%M:%S")))
 
 
 @pytest.fixture
 def browser(request):
-    driver = webdriver.Chrome()
+    logger.info("===================== Launching browser =====================")
+    driver = EventFiringWebDriver(webdriver.Chrome(), MyListener())
     driver.maximize_window()
 
     def teardown():
+        driver.save_screenshot("screenshot/OK-{0}.png".format(datetime.now().strftime("%d-%b-%Y %H:%M:%S")))
         driver.close()
+
     request.addfinalizer(teardown)
     return driver
