858c455c304ee987b8be208b95cfa98168121215
Igor Efimov <igor.efimov@merck.com>
2020-04-10 13:17:53 +0200

    DB check

--- dz23-db-check/conftest.py
+++ dz23-db-check/conftest.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+from mysql.connector import connect
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.events import EventFiringWebDriver, AbstractEventListener
 import allure
+import config
 import logging
 import pytest
 
@@ -59,6 +61,33 @@ def allure_attach_screenshot(driver):
     )
 
 
+def verify_db(request):
+    model = config.PRODUCT_MODEL
+    if request.keywords.node.name == 'test_02_update_product_model':
+        model = config.PRODUCT_MODEL_NEW
+    connection = connect(user='admin', password='admin', host='192.168.0.122', port='3306', database='opencart')
+    cursor = connection.cursor()
+    cursor.execute("SELECT oc_product_description.product_id, oc_product_description.name, oc_product.model "
+                   "FROM oc_product INNER JOIN oc_product_description "
+                   "ON oc_product_description.product_id=oc_product.product_id "
+                   "WHERE oc_product_description.name='{0}'"
+                   "AND oc_product.model='{1}';".format(config.PRODUCT_NAME, model))
+    result = cursor.fetchone()
+    connection.close()
+
+    print("Fetched following record from DB: {0}".format(result))
+
+    if request.keywords.node.name == 'test_01_add_new_product':
+        assert result, "[DB] product {0} is fetched".format(config.PRODUCT_NAME)
+        return
+
+    if request.keywords.node.name == 'test_03_remove_product_by_its_name':
+        assert not result, "[DB] product {0} is fetched".format(config.PRODUCT_NAME)
+        return
+
+    assert result, "[DB] product {0} is fetched".format(config.PRODUCT_NAME)
+
+
 @pytest.fixture
 def browser(request):
     log_file = request.config.getoption("--file")
@@ -83,6 +112,7 @@ def browser(request):
     driver.maximize_window()
 
     def teardown():
+        verify_db(request)
         allure_attach_screenshot(driver)
         if request.config.getoption("--browser-console-error"):
             browser_console_error_check(driver)
--- dz23-db-check/test3.py
+++ dz23-db-check/test3.py
@@ -3,45 +3,40 @@ from page_objects.DashBoardPage import DashBoardPage
 from page_objects.SecurityNotification import SecurityNotification
 from page_objects.ProductsPage import ProductsPage
 import allure
-
-PRODUCT_NAME = "Test_product"
-PRODUCT_TAG = "Test_tag"
-PRODUCT_MODEL = "Test_model"
-PRODUCT_MODEL_NEW = "Test new model"
-PRODUCT_IMAGE = 'jb.jpg'
+import config
 
 
-@allure.title("Add new product {0} to the system".format(PRODUCT_NAME))
+@allure.title("Add new product {0} to the system".format(config.PRODUCT_NAME))
 @allure.tag("adminSection", "add")
 @allure.severity(allure.severity_level.CRITICAL)
 def test_01_add_new_product(browser):
     AdminLoginPage(browser).login()
     SecurityNotification(browser).close()
     DashBoardPage(browser).navigate_to_products()
-    ProductsPage(browser).add_new_product(PRODUCT_NAME, PRODUCT_TAG, PRODUCT_MODEL, PRODUCT_IMAGE)
-    assert ProductsPage(browser).is_product_present(PRODUCT_NAME) is True, "{0} was added successfully".format(
-        PRODUCT_NAME)
+    ProductsPage(browser).add_new_product(config.PRODUCT_NAME, config.PRODUCT_TAG, config.PRODUCT_MODEL, config.PRODUCT_IMAGE)
+    assert ProductsPage(browser).is_product_present(config.PRODUCT_NAME) is True, "{0} was added successfully".format(
+        config.PRODUCT_NAME)
 
 
-@allure.title("Update products {0} model".format(PRODUCT_NAME))
+@allure.title("Update products {0} model".format(config.PRODUCT_NAME))
 @allure.tag("adminSection", "update")
 @allure.severity(allure.severity_level.NORMAL)
 def test_02_update_product_model(browser):
     AdminLoginPage(browser).login()
     SecurityNotification(browser).close()
     DashBoardPage(browser).navigate_to_products()
-    ProductsPage(browser).update_product_model(PRODUCT_NAME, PRODUCT_MODEL_NEW)
+    ProductsPage(browser).update_product_model(config.PRODUCT_NAME, config.PRODUCT_MODEL_NEW)
     assert ProductsPage(browser).get_product_model(
-        PRODUCT_NAME) == PRODUCT_MODEL_NEW, "{0} was updated successfully".format(PRODUCT_NAME)
+        config.PRODUCT_NAME) == config.PRODUCT_MODEL_NEW, "{0} was updated successfully".format(config.PRODUCT_NAME)
 
 
-@allure.title("Remove product {0} from the system".format(PRODUCT_NAME))
+@allure.title("Remove product {0} from the system".format(config.PRODUCT_NAME))
 @allure.tag("adminSection", "delete")
 @allure.severity(allure.severity_level.CRITICAL)
 def test_03_remove_product_by_its_name(browser):
     AdminLoginPage(browser).login()
     SecurityNotification(browser).close()
     DashBoardPage(browser).navigate_to_products()
-    ProductsPage(browser).delete_product_by_name(PRODUCT_NAME)
-    assert ProductsPage(browser).is_product_present(PRODUCT_NAME) is False, "{0} was removed successfully".format(
-        PRODUCT_NAME)
+    ProductsPage(browser).delete_product_by_name(config.PRODUCT_NAME)
+    assert ProductsPage(browser).is_product_present(config.PRODUCT_NAME) is False, "{0} was removed successfully".format(
+        config.PRODUCT_NAME)
